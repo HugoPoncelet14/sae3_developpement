e0e7b86a11ff3046328073b6781b71b5dd39c765
pino0017 <mathias.pinon@etudiant.univ-reims.fr>
2023-11-17 16:09:54 +0100

    Ajout de l'entité pays

---
+++ src/Entity/Pays.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PaysRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PaysRepository::class)]
+class Pays
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?int $idPays = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $idReg = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $nomPays = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdPays(): ?int
+    {
+        return $this->idPays;
+    }
+
+    public function setIdPays(int $idPays): static
+    {
+        $this->idPays = $idPays;
+
+        return $this;
+    }
+
+    public function getIdReg(): ?int
+    {
+        return $this->idReg;
+    }
+
+    public function setIdReg(?int $idReg): static
+    {
+        $this->idReg = $idReg;
+
+        return $this;
+    }
+
+    public function getNomPays(): ?string
+    {
+        return $this->nomPays;
+    }
+
+    public function setNomPays(string $nomPays): static
+    {
+        $this->nomPays = $nomPays;
+
+        return $this;
+    }
+}
