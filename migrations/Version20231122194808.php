4adfbbbb929a09ee6213b29df6a9aaddb8ba06da
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-11-22 20:50:10 +0100

    creation de la migration prennant en charge les nouvelles entités (Pays/Personne/Recette)

---
+++ migrations/Version20231122194808.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231122194808 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE pays (id INT AUTO_INCREMENT NOT NULL, nom_pays VARCHAR(50) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE personne (id INT AUTO_INCREMENT NOT NULL, nom_pers VARCHAR(50) NOT NULL, pnom_pers VARCHAR(50) DEFAULT NULL, sha512_pass VARCHAR(126) NOT NULL, date_nais DATETIME NOT NULL, pseudo VARCHAR(50) NOT NULL, photo_profil LONGBLOB DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE recette (id INT AUTO_INCREMENT NOT NULL, nom_rec VARCHAR(50) NOT NULL, desc_rec VARCHAR(250) NOT NULL, img_rec LONGBLOB DEFAULT NULL, tps_de_prep TIME NOT NULL, nbr_callo DOUBLE PRECISION NOT NULL, nbr_pers INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE pays');
+        $this->addSql('DROP TABLE personne');
+        $this->addSql('DROP TABLE recette');
+    }
+}
