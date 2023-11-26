c507e9741e4d35d0803f78ad620c9604c99860a8
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-11-26 12:35:49 +0100

    modification du load dans AllergeneFixtures

--- src/DataFixtures/AllergeneFixtures.php
+++ src/DataFixtures/AllergeneFixtures.php
@@ -11,6 +11,11 @@ class AllergeneFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        AllergeneFactory::createMany(20);
+        $dir = __DIR__;
+        $file = "$dir/data/Allergene.json";
+        $categories = json_decode(file_get_contents($file), true);
+        foreach ($categories as $value) {
+            AllergeneFactory::createOne($value);
+        }
     }
 }
