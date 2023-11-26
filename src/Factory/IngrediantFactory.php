9b86c9e773ad90426a95d7dae450f732c2331cbb
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-11-26 18:57:08 +0100

    modification du getDefaults de IngrediantFactory

--- src/Factory/IngrediantFactory.php
+++ src/Factory/IngrediantFactory.php
@@ -11,21 +11,21 @@ use Zenstruck\Foundry\RepositoryProxy;
 /**
  * @extends ModelFactory<Ingrediant>
  *
- * @method        Ingrediant|Proxy create(array|callable $attributes = [])
- * @method static Ingrediant|Proxy createOne(array $attributes = [])
- * @method static Ingrediant|Proxy find(object|array|mixed $criteria)
- * @method static Ingrediant|Proxy findOrCreate(array $attributes)
- * @method static Ingrediant|Proxy first(string $sortedField = 'id')
- * @method static Ingrediant|Proxy last(string $sortedField = 'id')
- * @method static Ingrediant|Proxy random(array $attributes = [])
- * @method static Ingrediant|Proxy randomOrCreate(array $attributes = [])
+ * @method        Ingrediant|Proxy                     create(array|callable $attributes = [])
+ * @method static Ingrediant|Proxy                     createOne(array $attributes = [])
+ * @method static Ingrediant|Proxy                     find(object|array|mixed $criteria)
+ * @method static Ingrediant|Proxy                     findOrCreate(array $attributes)
+ * @method static Ingrediant|Proxy                     first(string $sortedField = 'id')
+ * @method static Ingrediant|Proxy                     last(string $sortedField = 'id')
+ * @method static Ingrediant|Proxy                     random(array $attributes = [])
+ * @method static Ingrediant|Proxy                     randomOrCreate(array $attributes = [])
  * @method static IngrediantRepository|RepositoryProxy repository()
- * @method static Ingrediant[]|Proxy[] all()
- * @method static Ingrediant[]|Proxy[] createMany(int $number, array|callable $attributes = [])
- * @method static Ingrediant[]|Proxy[] createSequence(iterable|callable $sequence)
- * @method static Ingrediant[]|Proxy[] findBy(array $attributes)
- * @method static Ingrediant[]|Proxy[] randomRange(int $min, int $max, array $attributes = [])
- * @method static Ingrediant[]|Proxy[] randomSet(int $number, array $attributes = [])
+ * @method static Ingrediant[]|Proxy[]                 all()
+ * @method static Ingrediant[]|Proxy[]                 createMany(int $number, array|callable $attributes = [])
+ * @method static Ingrediant[]|Proxy[]                 createSequence(iterable|callable $sequence)
+ * @method static Ingrediant[]|Proxy[]                 findBy(array $attributes)
+ * @method static Ingrediant[]|Proxy[]                 randomRange(int $min, int $max, array $attributes = [])
+ * @method static Ingrediant[]|Proxy[]                 randomSet(int $number, array $attributes = [])
  */
 final class IngrediantFactory extends ModelFactory
 {
@@ -46,8 +46,23 @@ final class IngrediantFactory extends ModelFactory
      */
     protected function getDefaults(): array
     {
+        $faker = \Faker\Factory::create();
+        $faker->addProvider(new \FakerRestaurant\Provider\fr_FR\Restaurant($faker));
+
+        $nomIng = '';
+        $n = self::faker()->numberBetween(1, 6);
+        if (1 == $n) {
+            $nomIng = $faker->dairyName();
+        } elseif (2 == $n) {
+            $nomIng = $faker->vegetableName();
+        } elseif (3 == $n) {
+            $nomIng = $faker->fruitName();
+        } elseif (4 == $n) {
+            $nomIng = $faker->meatName();
+        }
+
         return [
-            'nomIng' => self::faker()->text(50),
+            'nomIng' => $nomIng,
         ];
     }
 
