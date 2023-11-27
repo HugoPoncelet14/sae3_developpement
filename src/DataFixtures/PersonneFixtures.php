0e22199613dd1b51e311414e276c960b85a70ada
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-11-27 22:37:46 +0100

    modification du load de PersonneFixtures

--- src/DataFixtures/PersonneFixtures.php
+++ src/DataFixtures/PersonneFixtures.php
@@ -3,14 +3,29 @@
 namespace App\DataFixtures;
 
 use App\Factory\PersonneFactory;
+use App\Factory\TypePersonneFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 ;
 
-class PersonneFixtures extends Fixture
+class PersonneFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        PersonneFactory::createMany(20);
+        PersonneFactory::createMany(10, function () {
+            $rep = ['typePersonne' => null];
+            if (PersonneFactory::faker()->boolean(90)) {
+                $rep = ['typePersonne' => TypePersonneFactory::random()];
+            }
+
+            return $rep;
+        }
+        );
+    }
+
+    public function getDependencies(): array
+    {
+        return [TypePersonneFixtures::class];
     }
 }
