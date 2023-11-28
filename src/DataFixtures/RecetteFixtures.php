701fc0e97f3a552685f991ea2cbb3dda6e6f84e6
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-11-28 19:56:24 +0100

    modification du load de RecetteFixtures

--- src/DataFixtures/RecetteFixtures.php
+++ src/DataFixtures/RecetteFixtures.php
@@ -3,14 +3,24 @@
 namespace App\DataFixtures;
 
 use App\Factory\RecetteFactory;
+use App\Factory\TypeRecetteFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 ;
 
-class RecetteFixtures extends Fixture
+class RecetteFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        RecetteFactory::createMany(10);
+        RecetteFactory::createMany(10, function () {
+            return ['typeRecette' => TypeRecetteFactory::random()];
+        }
+        );
+    }
+
+    public function getDependencies(): array
+    {
+        return [TypeRecetteFixtures::class];
     }
 }
