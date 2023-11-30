e24073d4232b665ed5baddec7fe2dca977f66195
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-11-30 16:36:29 +0100

    modification du load de PersonneFixtures (prise en compte de la relation avec Allergene)

--- src/DataFixtures/PersonneFixtures.php
+++ src/DataFixtures/PersonneFixtures.php
@@ -2,25 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\AllergeneFactory;
 use App\Factory\PersonneFactory;
 use App\Factory\TypePersonneFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
-;
 
 class PersonneFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         PersonneFactory::createMany(10, function () {
-            return ['typePersonne' => TypePersonneFactory::random()];
+            $nbr_All = AllergeneFactory::faker()->numberBetween(0, 3);
+            $list_All = [];
+            for ($i = 0; $i < $nbr_All; ++$i) {
+                $list_All[] = AllergeneFactory::random();
+            }
+
+
+            return ['typePersonne' => TypePersonneFactory::random(),
+                    'allergenes' => $list_All];
         }
         );
     }
 
     public function getDependencies(): array
     {
-        return [TypePersonneFixtures::class];
+        return [TypePersonneFixtures::class, AllergeneFixtures::class];
     }
 }
