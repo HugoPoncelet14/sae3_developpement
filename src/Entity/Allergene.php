d7f6393b9f100f6774e3e942242e7becb680e0d4
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-11-30 15:18:45 +0100

    generation de la relation ManyToMany entre Personne et Allergene

--- src/Entity/Allergene.php
+++ src/Entity/Allergene.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\AllergeneRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: AllergeneRepository::class)]
@@ -15,6 +17,14 @@ class Allergene
 
     #[ORM\Column(length: 50)]
     private ?string $nomAll = null;
+
+    #[ORM\ManyToMany(targetEntity: Personne::class, mappedBy: 'allergenes')]
+    private Collection $personnes;
+
+    public function __construct()
+    {
+        $this->personnes = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +39,33 @@ class Allergene
     public function setNomAll(string $nomAll): static
     {
         $this->nomAll = $nomAll;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Personne>
+     */
+    public function getPersonnes(): Collection
+    {
+        return $this->personnes;
+    }
+
+    public function addPersonne(Personne $personne): static
+    {
+        if (!$this->personnes->contains($personne)) {
+            $this->personnes->add($personne);
+            $personne->addAllergene($this);
+        }
+
+        return $this;
+    }
+
+    public function removePersonne(Personne $personne): static
+    {
+        if ($this->personnes->removeElement($personne)) {
+            $personne->removeAllergene($this);
+        }
 
         return $this;
     }
