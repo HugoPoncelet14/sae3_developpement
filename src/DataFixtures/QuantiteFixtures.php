8196e8efe1aa19dff2c9dc25c15000359d1f145e
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-11-30 17:32:44 +0100

    modification du load et des dependances de QuantiteFixtures

--- src/DataFixtures/QuantiteFixtures.php
+++ src/DataFixtures/QuantiteFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\IngrediantFactory;
 use App\Factory\QuantiteFactory;
 use App\Factory\RecetteFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -13,12 +14,13 @@ class QuantiteFixtures extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager): void
     {
         QuantiteFactory::createMany(20, function () {
-            return ['recette' => RecetteFactory::random()];
+            return ['recette' => RecetteFactory::random(),
+                    'ingrediant' => IngrediantFactory::random()];
         });
     }
 
     public function getDependencies(): array
     {
-        return [RecetteFixtures::class];
+        return [RecetteFixtures::class, IngrediantFixtures::class];
     }
 }
