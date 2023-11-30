c74ae2b666b1a104d94d6294675ecac5597d6fdc
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-11-30 16:56:46 +0100

    modification du load de IngrediantFixtures

--- src/DataFixtures/IngrediantFixtures.php
+++ src/DataFixtures/IngrediantFixtures.php
@@ -2,15 +2,17 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\AllergeneFactory;
 use App\Factory\IngrediantFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-;
 
 class IngrediantFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        IngrediantFactory::createMany(20);
+        IngrediantFactory::createMany(20, function () {
+            return ['allergene' => AllergeneFactory::random()];
+        });
     }
 }
