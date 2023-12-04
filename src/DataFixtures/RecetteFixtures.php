d413c3cd79e657e6c825310c7f544c2b02ec2d65
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-12-04 21:00:28 +0100

    modification du load de RecetteFixtures pour pouvoir utiliser les données du Json et mettre ces données en relation avec TypeRecette + supression d'un use dans RegionFixtures

--- src/DataFixtures/RecetteFixtures.php
+++ src/DataFixtures/RecetteFixtures.php
@@ -2,23 +2,28 @@
 
 namespace App\DataFixtures;
 
-use App\Factory\PaysFactory;
+use App\Entity\TypeRecette;
 use App\Factory\RecetteFactory;
 use App\Factory\TypeRecetteFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
-;
 
 class RecetteFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        RecetteFactory::createMany(10, function () {
-            return ['typeRecette' => TypeRecetteFactory::random(),
-                    'pays' => PaysFactory::random()];
+        $dir = __DIR__;
+        $file = "$dir/data/Recette.json";
+        $recettes = json_decode(file_get_contents($file), true);
+        foreach ($recettes as $infoRecette) {
+            $recette = ['nomRec' => $infoRecette['nomRec'],
+                        'tpsDePrep' => $infoRecette['tpsDePrep'],
+                        'tpsCuisson' => $infoRecette['tpsCuisson'],
+                        'nbrPers' => $infoRecette['nbrPers'],
+                        'typeRecette' => TypeRecetteFactory::random(['nomTpRec' => $infoRecette['nomTpRec']])];
+            RecetteFactory::createOne($recette);
         }
-        );
     }
 
     public function getDependencies(): array
