9f636f528cea660c9afcc0f7a25ac5839271b42b
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-12-05 14:09:25 +0100

    modification du load de PaysFixtures pour prendre en compte la relation avec Region

--- src/DataFixtures/PaysFixtures.php
+++ src/DataFixtures/PaysFixtures.php
@@ -3,19 +3,34 @@
 namespace App\DataFixtures;
 
 use App\Factory\PaysFactory;
+use App\Factory\RegionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 ;
 
-class PaysFixtures extends Fixture
+class PaysFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         $dir = __DIR__;
         $file = "$dir/data/Pays.json";
-        $pays = json_decode(file_get_contents($file), true);
-        foreach ($pays as $values) {
-            PaysFactory::createOne($values);
+        $infos = json_decode(file_get_contents($file), true);
+        foreach ($infos as $infoPays) {
+
+            $region = null;
+            if (isset($infoPays['nomReg'])) {
+                $region = RegionFactory::random(['nomReg' => $infoPays['nomReg']]);
+            }
+
+            $pays = ['nomPays' => $infoPays['nomPays'],
+                     'region' => $region];
+            PaysFactory::createOne($pays);
         }
+    }
+
+    public function getDependencies(): array
+    {
+        return [RegionFixtures::class];
     }
 }
