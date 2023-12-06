a591e08efad6b30fbb4962e00b56ab9a425010d9
Hugo Lherm <hugo.lherm@etudiant.univ-reims.fr>
2023-12-06 13:45:23 +0100

    Création de la route signup

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -9,10 +9,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class AuthenticationController extends AbstractController
 {
     #[Route('/auth', name: 'app_authentication')]
-    public function index(): Response
+    public function login(): Response
     {
         return $this->render('authentication/index.html.twig', [
             'controller_name' => 'AuthenticationController',
         ]);
     }
+
+    #[Route('/signup', name: 'app_authentication')]
+    public function signup(): Response
+    {
+        return $this->render('authentication/signup.html.twig');
+    }
+
 }
