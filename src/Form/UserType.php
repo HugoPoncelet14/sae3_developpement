c107dee40541370b753fedcc92ff9c97f5c2c2f8
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-12-14 14:56:40 +0100

    generation de la classe de formulaire de User

---
+++ src/Form/UserType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Allergene;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email')
+            ->add('roles')
+            ->add('password')
+            ->add('nom')
+            ->add('prenom')
+            ->add('dateNais')
+            ->add('pseudo')
+            ->add('photoProfil')
+            ->add('allergenes', EntityType::class, [
+                'class' => Allergene::class,
+'choice_label' => 'id',
+'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
