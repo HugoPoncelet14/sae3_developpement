53a0872c3cb3ff4c69c80b550f3fe36e84063416
Hugo Lherm <hugo.lherm@etudiant.univ-reims.fr>
2023-12-14 13:21:21 +0100

    Redirection de la route / vers /recettes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,13 +12,7 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(RecetteRepository $recetteRepository): Response
     {
-        $qb = $recetteRepository->createQueryBuilder('r');
-        $qb->where('r.id < 5');
-        $recettes = $qb->getQuery()->execute();
-        dump($recettes);
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-            'recettes' => $recettes,
-        ]);
+        return $this->redirectToRoute('app_recettes_index');
+
     }
 }
