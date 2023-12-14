89e5edbdbdc44e82d9bceaf32dcf817dff1720d0
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-12-14 14:03:41 +0100

    ajout de la structure des actions de crud en lies avec User

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-
-
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,5 +17,29 @@ class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
+    }
+
+    #[Route('/user/create')]
+    public function create(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        return $this->render('user/create.html.twig');
+    }
+
+    #[Route('/user/{id}', requirements: ['userId' => '\d+'])]
+    public function show(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        return $this->render('user/create.html.twig');
+    }
+
+    #[Route('user/{id}/update', requirements: ['userId' => '\d+'])]
+    public function update(EntityManagerInterface $entityManager, User $user, Request $request): Response
+    {
+        return $this->render('user/update.html.twig', ['user' => $user]);
+    }
+
+    #[Route('user/{id}/delete', requirements: ['userId' => '\d+'])]
+    public function delete(EntityManagerInterface $entityManager, User $user, Request $request): Response
+    {
+        return $this->render('user/delete.html.twig', ['user' => $user]);
     }
 }
