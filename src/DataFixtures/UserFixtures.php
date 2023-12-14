7d188771c99ec3ce6ac1c89d482394a5466c1876
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-12-14 14:51:40 +0100

    modification du load de UserFixtures pour attribuer a chaque user a sa creation entre 0 et 4 allergenes

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,10 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\AllergeneFactory;
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-;
 
 class UserFixtures extends Fixture
 {
@@ -15,9 +15,23 @@ class UserFixtures extends Fixture
         $file = "$dir/data/User.json";
         $users = json_decode(file_get_contents($file), true);
         foreach ($users as $value) {
+            $nbr_All = AllergeneFactory::faker()->numberBetween(0, 3);
+            $list_All = [];
+            for ($i = 0; $i < $nbr_All; ++$i) {
+                $list_All[] = AllergeneFactory::random();
+            }
+            $value['allergenes'] = $list_All;
             UserFactory::createOne($value);
         }
 
-        UserFactory::createMany(8);
+        UserFactory::createMany(8, function () {
+            $nbr_All = AllergeneFactory::faker()->numberBetween(0, 3);
+            $list_All = [];
+            for ($i = 0; $i < $nbr_All; ++$i) {
+                $list_All[] = AllergeneFactory::random();
+            }
+            return ['allergenes' => $list_All];
+
+        });
     }
 }
