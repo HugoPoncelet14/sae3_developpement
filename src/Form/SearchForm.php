b0270ba69826a94965e97b4b47896c3b4d8c6c4b
pino0017 <mathias.pinon@etudiant.univ-reims.fr>
2023-12-16 10:43:59 +0100

    Création de buildForm dans SearchForm.php

--- src/Form/SearchForm.php
+++ src/Form/SearchForm.php
@@ -4,11 +4,18 @@ declare(strict_types=1);
 
 namespace App\Form;
 
+use App\Entity\Pays;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SearchForm extends AbstractType
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+    }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
@@ -17,6 +24,7 @@ class SearchForm extends AbstractType
             'csrf_protection' => false,
         ]);
     }
+
     public function getBlockPrefix(): string
     {
         return '';
