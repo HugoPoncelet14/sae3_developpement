ccc36a56d1545b9ac7c6e09667234bf59e4d18ed
pino0017 <mathias.pinon@etudiant.univ-reims.fr>
2023-12-16 10:29:04 +0100

    Création de SearchData pour le filtre

---
+++ src/Data/SearchData.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+class SearchData
+{
+    public array $pays = [];
+
+    public array $region = [];
+    public array $typeRecette = [];
+}
