d7f67074ea355c97edbb15deb6e26b2e69bd27e7
pino0017 <mathias.pinon@etudiant.univ-reims.fr>
2023-12-16 10:34:48 +0100

    Création de la fonction ConfigureOptions dans SearchForm.php

--- src/Form/SearchForm.php
+++ src/Form/SearchForm.php
@@ -1,11 +1,20 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SearchForm extends AbstractType
 {
-
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+           'data_class' => SearchData::class,
+           'method' => 'GET',
+            'csrf_protection' => false,
+        ]);
+    }
 }
