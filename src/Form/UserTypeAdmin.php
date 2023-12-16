c2233d255836fd561987dd0b9e314985e6e68cba
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-12-16 19:06:17 +0100

    Creation du 2nd formulaire de User heritant de UserType (formulaire pour le role ROLE_ADMIN)

--- src/Form/UserTypeAdmin.php
+++ src/Form/UserTypeAdmin.php
@@ -2,62 +2,24 @@
 
 namespace App\Form;
 
-use App\Entity\Allergene;
 use App\Entity\User;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\File;
 
-class UserType extends AbstractType
+class UserTypeAdmin extends UserType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        parent::buildForm($builder, $options);
+
         $builder
-            ->add('email', EmailType::class)
-            ->add('nom', null, [
-                'empty_data' => '',
-            ])
-            ->add('prenom', null, [
-                'empty_data' => '',
-            ])
-            ->add('dateNais', DateType::class)
-            ->add('pseudo', null, [
-                'empty_data' => '',
-            ])
-            ->add('photoProfil', FileType::class, [
-                'required' => false,
-                'mapped' => false,
-                'constraints' => [
-                    new File([
-                        'mimeTypes' => ['image/jpeg', 'image/png'],
-                        'mimeTypesMessage' => 'Veuillez entrer un type mime valide (JPEG,PNG)',
-                    ]),
-                ],
-            ])
-            ->add('allergenes', EntityType::class, [
-                        'class' => Allergene::class,
-                        'choice_label' => 'nomAll',
-                        'multiple' => true,
-                        'expanded' => true,
-            ])
-            ->add('password', PasswordType::class, [
-                'empty_data' => '',
-                'required' => false,
-            ])
-            ->add('roles', ChoiceType::class, [
-                'multiple' => true,
-                'choices' => ['Admin' => 'ROLE_ADMIN'],
-                'expanded' => true,
-                'label' => 'Rôle',
-            ])
-        ;
+        ->add('roles', ChoiceType::class, [
+            'multiple' => true,
+            'choices' => ['Admin' => 'ROLE_ADMIN'],
+            'expanded' => true,
+            'label' => 'Rôle',
+        ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
