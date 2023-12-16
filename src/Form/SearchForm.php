cf63ee6ca06711b6d11ae7acbf4130e044c89c13
pino0017 <mathias.pinon@etudiant.univ-reims.fr>
2023-12-16 10:36:01 +0100

    Ajout du retour de la méthode configureOptions

--- src/Form/SearchForm.php
+++ src/Form/SearchForm.php
@@ -9,7 +9,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SearchForm extends AbstractType
 {
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
            'data_class' => SearchData::class,
