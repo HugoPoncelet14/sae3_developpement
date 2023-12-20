30764df8c9e2b29c71a43a03f993c0ceddbcc4cd
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-12-20 22:55:55 +0100

    ajout d'un premier test form() dans User/CreateCest

--- tests/Controller/User/CreateCest.php
+++ tests/Controller/User/CreateCest.php
@@ -6,12 +6,20 @@ use App\Tests\Support\ControllerTester;
 
 class CreateCest
 {
-    public function _before(ControllerTester $I)
+    public function form(ControllerTester $I): void
     {
-    }
-
-    // tests
-    public function tryToTest(ControllerTester $I)
-    {
+        $I->amOnPage('/user/create');
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeInTitle('Inscription');
+        $I->see('Inscription', 'h1');
+        $I->see('Email', 'label');
+        $I->see('Mot de passe', 'label');
+        $I->see('Répétez le mot de passe', 'label');
+        $I->see('Nom', 'label');
+        $I->see('Prenom', 'label');
+        $I->see('Pseudo', 'label');
+        $I->see('Date de naissance', 'legend');
+        $I->see('Photo de profil', 'label');
+        $I->see('Allergenes', 'legend');
     }
 }
