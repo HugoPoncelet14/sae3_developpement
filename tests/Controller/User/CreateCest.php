03b05d6f4588b3290d814f1d0c88112e388cce7f
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-12-20 21:30:44 +0100

    fin de la configuration de codeception et generation de User/CreateCest

---
+++ tests/Controller/User/CreateCest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tests\Controller\User;
+
+use App\Tests\Support\ControllerTester;
+
+class CreateCest
+{
+    public function _before(ControllerTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ControllerTester $I)
+    {
+    }
+}
