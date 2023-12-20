5453b02cb1cf5e34761a88cb536a306fa675103b
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2023-12-20 23:52:00 +0100

    ajout du test accessIsRestrictedToAdminUsersOnAdminUsers() dans User DeleteCest

--- tests/Controller/User/DeleteCest.php
+++ tests/Controller/User/DeleteCest.php
@@ -22,4 +22,28 @@ class DeleteCest
         $I->seeInTitle('Suppression de Stark, Tony');
         $I->see('Suppression de Stark, Tony', 'h1');
     }
+
+    public function accessIsRestrictedToAdminUsersOnAdminUsers(ControllerTester $I): void
+    {
+        $user = UserFactory::createOne(['prenom' => 'Tony',
+                'nom' => 'Stark',
+                'email' => 'root@example.com',
+                'roles' => ['ROLE_ADMIN']]
+        );
+
+        UserFactory::createOne(['prenom' => 'Tony',
+                'nom' => 'Stark',
+                'email' => 'ironman@example.com',
+                'roles' => ['ROLE_ADMIN']]
+        );
+
+        $realuser = $user->object();
+
+        $I->amLoggedInAs($realuser);
+
+        $I->amOnPage('/user/2/delete');
+
+        $I->seeCurrentRouteIs('app_recettes_index');
+    }
+
 }
