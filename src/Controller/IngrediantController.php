476fd6a281269c36ebcc1087091a62c7c8c5ed04
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2024-01-03 18:43:06 +0100

    Creation de la methode showUserImage dans le controller d'ingrediant

--- src/Controller/IngrediantController.php
+++ src/Controller/IngrediantController.php
@@ -17,4 +17,20 @@ class IngrediantController extends AbstractController
             'controller_name' => 'IngrediantController',
         ]);
     }
+
+    #[Route('/ingrediant/{id}/image', name: 'app_ingrediant_image')]
+    public function showUserImage(int $id, IngrediantRepository $ingrediantRepository)
+    {
+        $dir = __DIR__;
+        $ingrediant = $ingrediantRepository->findOneBy(['id' => $id]);
+        $response = new Response();
+        if (null === $ingrediant->getImgIng()) {
+            $response = new Response(file_get_contents("$dir/../../public/img/icone/ingrediant_base.png"));
+        } else {
+            $response = new Response(stream_get_contents($ingrediant->getImgIng()));
+        }
+        $response->headers->set('Content-Type', 'image/png');
+
+        return $response;
+    }
 }
