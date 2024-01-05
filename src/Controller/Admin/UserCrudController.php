af258ce89ffd4df7cdfd8ab66ee9955631491cc7
pino0017 <mathias.pinon@etudiant.univ-reims.fr>
2024-01-05 16:07:35 +0100

    Modification de configureFields pour le back-end

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,8 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Configurator\AssociationConfigurator;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
@@ -15,14 +18,19 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')
+            ->hideOnForm(),
+            TextField::new('prenom'),
+            TextField::new('nom'),
+            DateField::new('dateNais')
+            ->setDisabled(),
+            TextField::new('email')
+            ->setDisabled(),
+            ArrayField::new('roles'),
+            AssociationField::new('allergenes'),
         ];
     }
-    */
 }
