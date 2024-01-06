aa74e2c99074393c69567aaea78ae32cf611024f
pino0017 <mathias.pinon@etudiant.univ-reims.fr>
2024-01-06 18:52:07 +0100

    Modification de configureFields pour ajuster le back office de RecetteCrudController.php

--- src/Controller/Admin/RecetteCrudController.php
+++ src/Controller/Admin/RecetteCrudController.php
@@ -4,8 +4,11 @@ namespace App\Controller\Admin;
 
 use App\Entity\Recette;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AvatarField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class RecetteCrudController extends AbstractCrudController
@@ -15,14 +18,26 @@ class RecetteCrudController extends AbstractCrudController
         return Recette::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')
+                ->hideOnForm(),
+            TextField::new('nomRec'),
+            TextField::new('descRec')
+                ->hideOnIndex(),
+            AvatarField::new('imgRec')->setHeight(50),
+            IntegerField::new('tpsDePrep')
+                ->hideOnIndex(),
+            Field::new('nbrCallo')
+            ->hideOnIndex(),
+            AssociationField::new('typeRecette')
+                ->hideOnIndex(),
+            AssociationField::new('pays')
+                ->hideOnIndex(),
+
+            AssociationField::new('ustensiles')
+                ->hideOnIndex(),
         ];
     }
-    */
 }
