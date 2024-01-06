5aa028a70b93ca67bd19ca9f8d5978938068392a
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2024-01-06 19:59:08 +0100

    ajout de l'action create dans IngredientController

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -3,14 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Ingredient;
+use App\Form\IngredientType;
 use App\Repository\IngredientRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class IngredientController extends AbstractController
 {
-    #[Route('/ingrediet/{id}/image', name: 'app_ingredient_image')]
+    #[Route('/ingredient/{id}/image', name: 'app_ingredient_image')]
     public function showUserImage(int $id, IngredientRepository $ingredientRepository)
     {
         $dir = __DIR__;
@@ -26,9 +30,37 @@ class IngredientController extends AbstractController
         return $response;
     }
 
+    #[IsGranted('ROLE_ADMIN')]
+    #[Route('/ingredient/create', name: 'app_ingredient_create')]
+    public function create(EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $ingredient = new Ingredient();
+
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ingredient = $form->getData();
+
+            if (null !== $form->get('imgIng')->getData()) {
+                $imageFile = $form->get('imgIng')->getData();
+                $ingredient->setimgIng(file_get_contents($imageFile->getPathname()));
+            }
+
+            $entityManager->persist($ingredient);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_ingredient_show', ['id' => $ingredient->getId()]);
+        }
+
+        return $this->render('ingredient/create.html.twig', ['form' => $form]);
+    }
+
     #[Route('/ingredient/{id}', name: 'app_ingredient_show')]
     public function show(Ingredient $ingredient): Response
     {
         return $this->render('ingredient/show.html.twig', ['ingredient' => $ingredient]);
     }
+
+
 }
