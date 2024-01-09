d91a84af508ddd17447afc270c1fa7d0160f4649
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2024-01-09 19:07:05 +0100

    creation du fichier de formulaire EtapeType pour le crud de recette + creation du recette/_form

---
+++ src/Form/EtapeType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Etape;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EtapeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        for ($i = 1; $i <= $options['nbrEtapes']; ++$i) {
+            $builder
+                ->add("descEtape$i", TextAreaType::class, [
+                    'label' => "Etape $i",
+                ])
+            ;
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => null,
+            'nbrEtapes' => false,
+        ]);
+    }
+}
