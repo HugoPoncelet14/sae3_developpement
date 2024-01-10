be5c030e60913d8d64ed4c1c316369664d716511
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2024-01-10 19:39:41 +0100

    ajout d'un nouveau test (testSubmitFormPage2) dans recette/CreateCest

--- tests/Controller/recette/CreateCest.php
+++ tests/Controller/recette/CreateCest.php
@@ -120,4 +120,44 @@ class CreateCest
         $I->see('Quantité', 'label');
         $I->see('Unité de mesure', 'label');
     }
+
+    public function testSubmitFormPage2(ControllerTester $I)
+    {
+        UstensileFactory::createSequence([['name' => 'UstensileTest1'], ['name' => 'UstensileTest2']]);
+        IngredientFactory::createSequence([['nomIng' => 'IngredientTest1'], ['nomIng' => 'IngredientTest2']]);
+
+        $user = UserFactory::createOne(['prenom' => 'Tony',
+                'nom' => 'Stark',
+                'email' => 'ironman@example.com',
+                'roles' => ['ROLE_ADMIN']]
+        );
+        $realuser = $user->object();
+        $I->amLoggedInAs($realuser);
+
+        $I->amOnPage('/recette/create');
+
+        $I->submitForm('form[name="recette"]', [
+            'recette[nomRec]' => 'Recette Test',
+            'recette[descRec]' => 'Description Test',
+            'recette[tpsDePrep]' => 20,
+            'recette[tpsCuisson]' => 30,
+            'recette[nbrCallo]' => 1500,
+            'recette[nbrPers]' => 4,
+            'recette[typeRecette]' => TypeRecetteFactory::createOne(),
+            'recette[pays]' => PaysFactory::createOne(),
+            'recette[ustensiles]' => [1, 2],
+            'recette[ingredients]' => [1, 2],
+            'recette[nbrEtapes]' => 4,
+        ], 'input[type="submit"]');
+
+        $I->submitForm('form[name="quantite"]', [
+            'quantite[quantiteIng1]' => 100,
+            'quantite[unitMesureIng1]' => 'cl',
+            'quantite[quantiteIng2]' => 100,
+            'quantite[unitMesureIng2]' => 'g',
+        ], 'input[type="submit"]');
+
+        $I->seeCurrentRouteIs('app_recette_createEtp');
+        $I->seeResponseCodeIsSuccessful();
+    }
 }
