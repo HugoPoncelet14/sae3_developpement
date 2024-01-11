725a75f9e1c31c074ab903faea15ab33a96a1385
pino0017 <mathias.pinon@etudiant.univ-reims.fr>
2024-01-11 14:39:21 +0100

    Modification de SearchData.php.php pour ajout les filtres avec les allergènes de l'utilisateur

--- src/Data/SearchData.php
+++ src/Data/SearchData.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace App\Form;
 
+use App\Entity\User;
+
 class SearchData
 {
     public int $page = 1;
@@ -12,4 +14,7 @@ class SearchData
 
     public array $region = [];
     public array $typeRecette = [];
+    public bool $allergene = false;
+    public User $user;
+
 }
