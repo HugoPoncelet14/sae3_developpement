48e5d2c5dbd1878da9cb313b7399c9c384e63e2a
pino0017 <mathias.pinon@etudiant.univ-reims.fr>
2024-01-11 14:48:12 +0100

    Modification de EtapeCrudController.php pour le DashBoard

--- src/Controller/Admin/EtapeCrudController.php
+++ src/Controller/Admin/EtapeCrudController.php
@@ -4,8 +4,9 @@ namespace App\Controller\Admin;
 
 use App\Entity\Etape;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class EtapeCrudController extends AbstractCrudController
@@ -18,9 +19,11 @@ class EtapeCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')
+            ->hideOnForm(),
+            IntegerField::new('numEtape'),
+            TextField::new('descEtape'),
+            AssociationField::new('recette'),
         ];
     }
 }
