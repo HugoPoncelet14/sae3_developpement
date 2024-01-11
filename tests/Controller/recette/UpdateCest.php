4a4f90cb5988f8a34f6a96e4631c89203a6c9f1b
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2024-01-11 01:29:46 +0100

    ajout d'un nouveau test (testStructurePage2) dans recette/UpdateCest

--- tests/Controller/recette/UpdateCest.php
+++ tests/Controller/recette/UpdateCest.php
@@ -98,4 +98,55 @@ class UpdateCest
         $I->seeCurrentRouteIs('app_recette_updateQte');
         $I->seeResponseCodeIsSuccessful();
     }
+
+    public function testStructurePage2(ControllerTester $I)
+    {
+        $recette = RecetteFactory::createOne(['tpsCuisson' => 10,
+            'pays' => PaysFactory::createOne(['nomPays' => 'PaysTest']),
+            'typeRecette' => TypeRecetteFactory::createOne(['nomTpRec' => 'TypeRecetteTest']),
+            'ustensiles' => UstensileFactory::createSequence([['name' => 'UstensileTest1'], ['name' => 'UstensileTest2']])]);
+        QuantiteFactory::createOne(['recette' => $recette,
+            'quantite' => 100,
+            'unitMesure' => 'unitTest',
+            'ingredient' => IngredientFactory::createOne(['nomIng' => 'IngredientTest1'])]);
+        EtapeFactory::createSequence([['recette' => $recette, 'numEtape' => 1, 'descEtape' => 'DescTest1'], ['recette' => $recette, 'numEtape' => 2, 'descEtape' => 'DescTest2']]);
+
+        $user = UserFactory::createOne(['prenom' => 'Tony',
+                'nom' => 'Stark',
+                'email' => 'ironman@example.com',
+                'roles' => ['ROLE_ADMIN']]
+        );
+        $realuser = $user->object();
+        $I->amLoggedInAs($realuser);
+
+        UstensileFactory::createSequence([['name' => 'UstensileTest3'], ['name' => 'UstensileTest4']]);
+        IngredientFactory::createSequence([['nomIng' => 'IngredientTest2'], ['nomIng' => 'IngredientTest3']]);
+
+        $I->amOnPage('/recette/1/update');
+
+        $I->submitForm('form[name="recette"]', [
+            'recette[nomRec]' => 'Recette Test Modifiee',
+            'recette[descRec]' => 'Description Test Modifiee',
+            'recette[tpsDePrep]' => 60,
+            'recette[tpsCuisson]' => 60,
+            'recette[nbrCallo]' => 2500,
+            'recette[nbrPers]' => 6,
+            'recette[typeRecette]' => TypeRecetteFactory::createOne(['nomTpRec' => 'TypeRecetteTest Modifié'])->getId(),
+            'recette[pays]' => PaysFactory::createOne(['nomPays' => 'PaysTest Modifié'])->getId(),
+            'recette[ustensiles]' => [1, 3, 4],
+            'recette[ingredients]' => [2, 3],
+            'recette[nbrEtapes]' => 3,
+        ], 'input[type="submit"]');
+
+        $I->seeInTitle('Ajout des quantités pour les ingrédients');
+        $I->see('Ajout des quantités pour les ingrédients', 'h1');
+
+        $I->see('IngredientTest2', 'h2');
+        $I->see('Quantité', 'label');
+        $I->see('Unité de mesure', 'label');
+
+        $I->see('IngredientTest3', 'h2');
+        $I->see('Quantité', 'label');
+        $I->see('Unité de mesure', 'label');
+    }
 }
