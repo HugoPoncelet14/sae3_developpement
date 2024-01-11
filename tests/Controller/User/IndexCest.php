331f06ca346b0e98ddb9869e0f2e4d6cc78ff652
Hugo Poncelet <hugo.poncelet1@etudiant.univ-reims.fr>
2024-01-11 22:09:30 +0100

    ajout d'un nouveau test (testTri) dans user/IndexCest

--- tests/Controller/User/IndexCest.php
+++ tests/Controller/User/IndexCest.php
@@ -47,4 +47,22 @@ class IndexCest
         $I->seeResponseCodeIsSuccessful();
         $I->seeCurrentRouteIs('app_user_show');
     }
+
+    public function testTri(ControllerTester $I): void
+    {
+        $user = UserFactory::createOne([
+            'prenom' => 'Homer',
+            'nom' => 'Simpson',
+            'roles' => ['ROLE_ADMIN'],
+        ]);
+
+        $realuser = $user->object();
+        $I->amLoggedInAs($realuser);
+
+        UserFactory::createSequence([['nom' => 'aNom', 'prenom' => 'prenom1'], ['nom' => 'cNom', 'prenom' => 'prenom2'], ['nom' => 'bNom', 'prenom' => 'prenom3'], ['nom' => 'dNom', 'prenom' => 'prenom4']]);
+        $I->amOnPage('/user');
+        $I->seeResponseCodeIsSuccessful();
+        $links = $I->grabMultiple('li a.user');
+        $I->assertEquals($links, ['aNom prenom1', 'bNom prenom3', 'cNom prenom2', 'dNom prenom4']);
+    }
 }
